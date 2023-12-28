30f1bd58c1de427486a1323a4c4619ef3c851bc6
Adam Stankiewicz <41164873+AdamJStankiewicz@users.noreply.github.com>
2023-12-27 23:01:53 -0500

    Input State and City
    Input state and city, to then find temperature. Saves info locally!

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var curTemperature;
 
+function init() {
+    console.log("INITIALIZED!");
+    var city = localStorage['city'];
+    var state = localStorage['state'];
+
+    var cityInput = document.getElementById("city");
+    var stateInput = document.getElementById("state");
+
+    cityInput.setAttribute('value',city);
+    stateInput.setAttribute('value',state);
+    console.log("INITIALIZED!");
+}
+
+function fillInfo(city,state){
+    document.getElementById("city").value = city;
+}
+
 function weatherClick(dwn){
     const btn = document.getElementById("wtherBtn");
     if(dwn){
@@ -19,7 +36,16 @@ function removeAnim(){
 }
 
 async function getWeather(){
-    var latlong = await getCoordinates();
+    var cityInput = document.getElementById("city").value;
+    var stateInput = document.getElementById("state").value.toUpperCase();
+    localStorage['city'] = cityInput;
+    localStorage['state'] = stateInput;
+
+    if(cityInput === "" || stateInput === ""){
+        return;
+    }
+    console.log(cityInput);
+    var latlong = await getCoordinates(cityInput,stateInput);
     var lat = latlong[0], 
         long = latlong[1];
 
@@ -30,16 +56,16 @@ async function getWeather(){
 
     obj = await res.json();
 
-    curTemperature = obj["main"]["temp"];
+    curTemperature = Math.round(obj["main"]["temp"]);
 
     document.getElementById("temperature").textContent = "The current temperature is: " +  curTemperature + "F";
+    //document.getElementById("latlong").textContent = "Lat: " + lat + " Long: " + long;
 }
 
-async function getCoordinates() {
-    const apiUrl = await fetch("https://us1.locationiq.com/v1/search?key=pk.143ca4fe36f259c3ed41b80e7ddb7835&q=Lousville%2C%20KY&format=json");
+async function getCoordinates(city,state) {
+    const apiUrl = await fetch("https://us1.locationiq.com/v1/search?key=pk.143ca4fe36f259c3ed41b80e7ddb7835&q="+city+"%2C%20"+state+"&format=json");
     let data = await apiUrl.json();
     let latitude = data[0].lat;
     let longitude = data[0].lon;
-    document.getElementById("latlong").textContent = "Lat: " + latitude + " Long: " + longitude;
     return[latitude,longitude];
 }
